7d86882f1767c38f170c25d0ab8c8d73ba32c6bf
DESKTOP-HFGLGK0\Jimin Chung <bellator0406@gmail.com>
2016-11-01 08:35:03 -0400

    Added the screen shake effect. <NOTE: SCREEN SHAKE WITH P3 DOES NOT WORK AT THE MOMENT>

---
+++ 1dGame-master/1dGame/Assets/Scripts/cameraShake.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraShake : MonoBehaviour {
+
+    Vector3 initialL;
+    Quaternion initialR;
+
+    public float shakeDuration = 0.1f;
+    public float magnitude = 0.1f;
+
+    // Use this for initialization
+    void Start()
+   {
+        initialL = transform.position;
+        initialR = transform.rotation;
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+            {
+            StartCoroutine("shakeCam");
+            }
+
+        if (Input.GetKeyDown(KeyCode.L))
+            {
+            StartCoroutine("shakeCam");
+            }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CollisionH")
+        {
+            StartCoroutine("shakeCam");
+        }
+
+        else if (other.gameObject.tag == "CollisionW")
+        {
+            StartCoroutine("shakeCam");
+        }
+
+    }
+
+
+
+
+
+            IEnumerator shakeCam()
+    {
+        float timeLeft = shakeDuration;
+        while (timeLeft > 0)
+        {
+            transform.position = initialL + Random.insideUnitSphere * magnitude;
+            timeLeft -= Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = initialL;
+        transform.rotation = initialR;
+    }
+}
+
+
